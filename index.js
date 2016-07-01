ea830d8c6410e1339868ee960c6cf33e5aedee05
kelvv <kelvv@outlook.com>
2016-07-02 02:53:37 +0800

    add regex replace

--- index.js
+++ index.js
@@ -42,7 +42,6 @@ Regex.prototype.match = function (str) {
     if(!match){
         return match;
     }
-    console.log(match);
     var result = {};
     result.value = match[0];
     result.index = match.index;
@@ -82,4 +81,50 @@ Regex.prototype.matches = function (str) {
     }
 
     return result;
+}
+
+/**
+ * Define replace 
+ * 
+ * @param {string} str
+ * @param {function} or {string} regexcer
+ */
+
+Regex.prototype.replace = function (str,regexcer) {
+    
+    var result = '';
+    var index = 0;
+    var lastWord = '';
+    while(match = this.regex.exec(str)){
+        var matchObj = {
+            value : match[0]
+        }
+        matchObj.groups = [];
+        matchObj.index = match.index;
+        matchObj.input = match.input;
+        for(var i=1;i<match.length;i++){
+            matchObj.groups.push({
+                value : match[i]
+            })
+        }
+        var afterStr = '';
+        var replaceValue = '';
+        if(typeof(regexcer)==='function'){
+            replaceValue = regexcer(matchObj);
+        }else{
+            replaceValue = regexcer;
+        }
+        var handleStr = str.substr(index,match.index+match[0].length-index);
+        afterStr = handleStr.replace(new RegExp(this.regex.source),replaceValue);
+        lastWord = str.substr(match.index+match[0].length,str.length-(match.index+match[0].length));
+        result = result + afterStr;
+        index = match.index+match[0].length;
+    }
+
+    result = result + lastWord;
+
+    result = result===''?str:result;
+
+    return result;
+
 }
--- test/default.js
+++ test/default.js
@@ -3,8 +3,17 @@ var Regex = require('../');
 
 var regex = new Regex('(W)(1)(2)3','ig');
 
+var regex2 = new Regex('H','ig');
+
 console.log(regex.test('2'));
 
 console.log(JSON.stringify(regex.match('wWW')));
 
 console.log(JSON.stringify(regex.matches('Vwisit W123School')));
+
+
+var result =regex2.replace('Visit Hello','Q');
+
+
+
+console.log(result);
