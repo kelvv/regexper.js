5fbdde3f899b90d4f84b61c52ae77258245a3790
kelvv <kelvv@outlook.com>
2016-12-19 11:42:15 +0800

    code styel for standard

--- index.js
+++ index.js
@@ -3,126 +3,123 @@
  * Expose the root regex
  */
 
-module.exports = Regex;
-
+module.exports = Regex
 
 /**
  * Define regex Fuc
- * 
+ *
  * @param {string} regexp
  * @param {string} flags
  */
 
-function Regex(regexp,flags) {
-    try{
-        this.regex = new RegExp(regexp,flags);
-    }catch(err){
-        throw err;
-    }
+function Regex (regexp, flags) {
+  try {
+    this.regex = new RegExp(regexp, flags)
+  } catch (err) {
+    throw err
+  }
 }
 
 /**
- * Define test 
- * 
+ * Define test
+ *
  * @param {string} content
  */
 
 Regex.prototype.test = function (content) {
-    return this.regex.test(content);
+  return this.regex.test(content)
 }
 
 /**
- * Define match 
- * 
+ * Define match
+ *
  * @param {string} str
  */
 
 Regex.prototype.match = function (str) {
-    var match = this.regex.exec(str);
-    if(!match){
-        return match;
-    }
-    var result = {};
-    result.value = match[0];
-    result.index = match.index;
-    result.groups = [];
-    for(i=1;i<match.length;i++){
-        result.groups.push(match[i])
-    }
-    return result;
+  var match = this.regex.exec(str)
+  if (!match) {
+    return match
+  }
+  var result = {
+    value: match[0],
+    index: match.index,
+    groups: []
+  }
+  for (var m of match) {
+    result.groups.push(m)
+  }
+  return result
 }
 
-
 /**
- * Define matches 
- * 
+ * Define matches
+ *
  * @param {string} str
  */
 
 Regex.prototype.matches = function (str) {
-    var result = [];
-    var match = {};
-    while(match = this.regex.exec(str)){
-        var matchObj = {
-            value : match[0]
-        }
-        matchObj.groups = [];
-        matchObj.index = match.index;
-        for(var i=1;i<match.length;i++){
-            matchObj.groups.push(match[i])
-        }
-        result.push(matchObj);
-        if(!this.regex.global){
-            break;
-        }
+  var result = []
+  var match = {}
+  while ((match = this.regex.exec(str))) {
+    var matchObj = {
+      value: match[0],
+      index: match.index,
+      groups: []
     }
+    for (var m of match) {
+      matchObj.groups.push(m)
+    }
+    result.push(matchObj)
+    if (!this.regex.global) {
+      break
+    }
+  }
 
-    return result;
+  return result
 }
 
 /**
- * Define replace 
- * 
+ * Define replace
+ *
  * @param {string} str
  * @param {function} or {string} regexcer
  */
 
-Regex.prototype.replace = function (str,regexcer) {
-    
-    var result = '';
-    var index = 0;
-    var lastWord = '';
-    var match = {};
-    while(match = this.regex.exec(str)){
-        var matchObj = {
-            value : match[0]
-        }
-        matchObj.groups = [];
-        matchObj.index = match.index;
-        for(var i=1;i<match.length;i++){
-            matchObj.groups.push(match[i])
-        }
-        var afterStr = '';
-        var replaceValue = '';
-        if(typeof(regexcer)==='function'){
-            replaceValue = regexcer(matchObj);
-        }else{
-            replaceValue = regexcer;
-        }
-        var handleStr = str.substr(index,match.index+match[0].length-index);
-        afterStr = handleStr.replace(new RegExp(this.regex.source),replaceValue);
-        lastWord = str.substr(match.index+match[0].length,str.length-(match.index+match[0].length));
-        result = result + afterStr;
-        index = match.index+match[0].length;
-        if(!this.regex.global){
-            break;
-        }
+Regex.prototype.replace = function (str, regexcer) {
+  var result = ''
+  var index = 0
+  var lastWord = ''
+  var match = {}
+  while ((match = this.regex.exec(str))) {
+    var matchObj = {
+      value: match[0],
+      index: match.index,
+      groups: []
     }
+    for (var m of match) {
+      matchObj.groups.push(m)
+    }
+    var afterStr = ''
+    var replaceValue = ''
+    if (typeof (regexcer) === 'function') {
+      replaceValue = regexcer(matchObj)
+    } else {
+      replaceValue = regexcer
+    }
+    var handleStr = str.substr(index, match.index + match[0].length - index)
+    afterStr = handleStr.replace(new RegExp(this.regex.source), replaceValue)
+    lastWord = str.substr(match.index + match[0].length, str.length - (match.index + match[0].length))
+    result = result + afterStr
+    index = match.index + match[0].length
+    if (!this.regex.global) {
+      break
+    }
+  }
 
-    result = result + lastWord;
+  result = result + lastWord
 
-    result = result===''?str:result;
+  result = result === '' ? str : result
 
-    return result;
-
+  return result
 }
